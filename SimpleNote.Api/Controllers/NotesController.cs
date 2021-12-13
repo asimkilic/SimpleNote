3f1b76d7b994c6fc2d9b389bc52452db2d78779f
asimkilic <a.asim.kilic@gmail.com>
2021-12-13 12:53:34 +0300

    Add project files.

---
+++ SimpleNote.Api/Controllers/NotesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleNote.Api.Data;
+using SimpleNote.Api.Dtos.Note;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleNote.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public NotesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Note>> GetNotes()
+        {
+            return await _context.Notes.OrderByDescending(x => x.Time).ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Note>> GetNote(int id)
+        {
+            Note note = await _context.Notes.FindAsync(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+            return note;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Note>> PostNote(PostNoteDto postNoteDto)
+        {
+            if (ModelState.IsValid)
+            {
+                Note note = new() { Content = postNoteDto.Content, Title = postNoteDto.Title };
+                _context.Add(note);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction(nameof(GetNote), new { id = note.Id }, note);
+            }
+            return BadRequest(ModelState);
+        }
+        // PUT: api/Notes/3
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Note>> PutNote(int id, PutNoteDto putNoteDto)
+        {
+            if (id != putNoteDto.Id) return BadRequest();
+
+            if (ModelState.IsValid)
+            {
+
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
